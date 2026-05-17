b3691e2eca9ae7a61f7a75f0d75d26866cf2fe81
TnTBass <tyler.jurgens@gmail.com>
2026-05-17 17:51:10 -0600

    chore: scaffold Fabric mod

---
+++ src/client/java/com/explosive/carrybabyanimals/client/CarryBabyAnimalsClient.java
@@ -0,0 +1,9 @@
+package com.explosive.carrybabyanimals.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class CarryBabyAnimalsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
